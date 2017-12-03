755113007a8ae1d78bfa8acf3175651676e14403
EthanTang0115 <tangyichuan18@gmail.com>
2017-12-02 20:49:27 -0500

    ethan_test_satur

---
+++ skeleton_joint_mapping.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+"""
+this program provides function to map 3D positions of kinect skeleton points to
+joint rotation angles.
+
+skeleton points (8): right_shoulder, left_shoulder, left_hip, elbow, wrist, hand, thumb, handtip
+each points should be the the form of 1 by 2 numpy array
+
+joint rotations (7): shoulder_yaw, shoulder_pitch, shoulder_roll, elbow_pitch, elbow_roll, hand_yaw, wrist_yaw
+each rotation value is a scalar
+
+"""
+
+def points_rotation_compute(right_shoulder):
+
+
+    return shoulder_yaw, shoulder_pitch, shoulder_roll, elbow_pitch, elbow_roll, hand_yaw, wrist_yaw
+
+def skeleton_points_generation(shoulder_yaw, shoulder_pitch, shoulder_roll, elbow_pitch, elbow_roll, hand_taw, wrist_yaw):
+
+
+    return right_shoulder, left_shoulder, left_hip, elbow, wrist, hand, thumb, handtip
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
